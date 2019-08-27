60c9534256b637748789e0e3a7ab3f26fef87fbd
Niranjan kumar <gowithnk@gmail.com>
2019-08-27 11:17:25 +0530

    Updates

---
+++ precache-manifest.39a94d0ec2615b71b0e1df82873629e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34ea6e5264e3ba459ace7007d8ac628",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a51b52fb66c5723673f",
+    "url": "/monsters-rolodex/static/css/main.6b51504c.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/monsters-rolodex/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "5a51b52fb66c5723673f",
+    "url": "/monsters-rolodex/static/js/main.524dcb29.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
